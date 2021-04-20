75f65342a0ce2ff77fc3a66387c47ee20d3283f7
reenadebray <42624481+reenadebray@users.noreply.github.com>
2021-04-20 14:48:55 -0700

    Update 16S_sequence_processing.R

--- 16S_sequence_processing.R
+++ 16S_sequence_processing.R
@@ -5,6 +5,7 @@
 # Load packages
 library(dada2)
 library(phyloseq)
+library(decontam)
 
 # Initialize "path" variable as directory containing fastq files
 ag_fnFs <- sort(list.files(path, pattern="_R1_001.fastq.gz", full.names = TRUE))
@@ -54,3 +55,34 @@ ag_OTU = otu_table(ag_asv_tab, taxa_are_rows = TRUE)
 ag_TAX = tax_table(ag_taxa)
 ag_samples = ag_sample_info
 ag_ps <-phyloseq(ag_OTU, ag_TAX, ag_samples)
+
+# Remove chloroplast and mitochondrial sequences & PCR positive control
+ag_ps_nochloro <- subset_taxa(ag_ps, (Order!="Chloroplast" | is.na(Order))) #important to specify is.na() here, or phyloseq will remove NA taxa as well
+ag_ps_NoCNoM <- subset_taxa(ag_ps_nochloro, (Family!="Mitochondria" | is.na(Family))) #important to specify is.na() here, or phyloseq will remove NA taxa as well
+ag_ps_NoCNoM_nocontrols <- subset_samples(ag_ps_NoCNoM, Group=="Field" | Group=="Genotype experiment" | Group=="Phosphorous experiment")
+ag_ps_rmpos = subset_samples(ag_ps_NoCNoM, Group != "positive control") #includes sequencing controls, extraction controls, buffer, & fertilizer
+ag_samples_df <- as.data.frame(sample_data(ag_ps_rmpos))
+
+# Run decontam function on PCR negative controls
+ag_ps_rmpos_negdecontam <- subset_samples(ag_ps_rmpos, (Group!="Extraction control" & Treatment.ID!="(sampling buffer)") | is.na(Treatment.ID))
+sample_data(ag_ps_rmpos_negdecontam)$is.neg <- (sample_data(ag_ps_rmpos_negdecontam)$Group == "negative control")
+contamdf.prev1 <- isContaminant(ag_ps_rmpos_negdecontam, method="prevalence", neg="is.neg", threshold=0.1)
+contam_1<-which(contamdf.prev1$contaminant)
+
+# Run decontam on buffer negative control
+ag_ps_rmpos_buffer<- subset_samples(ag_ps_rmpos, (Group!="Extraction control" & !is.na(Treatment.ID)))
+sample_data(ag_ps_rmpos_buffer)$is.neg <- (sample_data(ag_ps_rmpos_buffer)$Treatment.ID == "(sampling buffer)")
+contamdf.prev2 <- isContaminant(ag_ps_rmpos_buffer, method="prevalence", neg="is.neg", threshold=0.1)
+contam_2<-which(contamdf.prev2$contaminant)
+
+# Run decontam on DNA extraction negative controls, blocked by extraction batch
+ag_ps_rmpos_extraction<- subset_samples(ag_ps_rmpos, (Treatment.ID != "(sampling buffer)" & !is.na(Treatment.ID)))
+sample_data(ag_ps_rmpos_extraction)$is.neg <- (sample_data(ag_ps_rmpos_extraction)$Group == "Extraction control")
+sample_data(ag_ps_rmpos_extraction)$batch<-c(rep("B1",24),rep("B2",24),rep("B3",23),rep("B4",24),rep("B5",24),rep("B6",19),rep("B7",24))
+contamdf.prev3 <- isContaminant(ag_ps_rmpos_extraction, method="prevalence",batch="batch", neg="is.neg", threshold=0.1)
+contam_3<-which(contamdf.prev3$contaminant)
+
+# Merge contaminant lists from different negative controls and filter from data
+ag_badTaxa = taxa_names(ag_ps_rmpos)[sort(unique(c(contam_1,contam_2,contam_3)))]
+ag_goodTaxa <- setdiff(taxa_names(ag_ps_NoCNoM_nocontrols), ag_badTaxa)
+ag_ps_nocontam_allsamp = prune_taxa(ag_goodTaxa, ag_ps_NoCNoM_nocontrols)
